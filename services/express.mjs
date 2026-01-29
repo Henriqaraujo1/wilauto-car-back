cdc792d580b45e437a75703431bb37d8af594416
Henrique <henriqaraujo1@gmail.com>
2026-01-29 15:58:56 -0300

    correção cors

--- services/express.mjs
+++ services/express.mjs
@@ -1,26 +1,26 @@
-import pkg from "body-parser"
+import pkg from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import session from "express-session";
-import compression from 'compression';
+import compression from "compression";
 import errorHandler from "../middlewares/errorServices.mjs"; // Importa o middleware de erro
-let  { json, urlencoded } = pkg;
+let { json, urlencoded } = pkg;
 
-
-dotenv.config({path: "/git/system-back/back/.env.production"})
+dotenv.config({ path: "/git/system-back/back/.env.production" });
 
 /* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
-export default (app) =>{
+export default (app) => {
   let corsOptions = {
-    origin: '*',
-    methods: 'GET,PUT,POST,DELETE'
+    origin: "https://will-auto.worldsoft-inc.com",
+    methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+    credentials: true,
   };
 
   app.use(
     compression({
       level: 9, // Nível de compactação (0-9). Padrão é 6.
       threshold: 1024, // Compacta apenas respostas maiores que 1KB
-    })
+    }),
   );
 
   app.use(cors(corsOptions));
@@ -40,7 +40,7 @@ export default (app) =>{
         secure: true,
         maxAge: 24 * 60 * 60 * 1000,
       },
-    })
+    }),
   );
 
   // app.use(errorHandler);
